f359c3ab3169580dc9bb24a2b66cba0167af36a7
Jimmy Murray <SeeJimmyMurray@gmail.com>
2024-03-19 15:02:12 -0400

    Update App.jsx

--- FullStackExercises/osa2/2.05/App.jsx
+++ FullStackExercises/osa2/2.05/App.jsx
@@ -5,35 +5,6 @@ import Content from './Content';
 import Part from './Part';
 import Total from './Total';
 
-const Header = ({ courseName }) => {
-  return <h1>{courseName}</h1>;
-};
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(part => (
-        <Part key={part.id} part={part} />
-      ))}
-    </div>
-  );
-};
-
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  );
-};
-
-const Total = ({ parts }) => {
-  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
-  return (
-    <p><strong>Total of {totalExercises} exercises</strong></p>
-  );
-};
-
 const App = () => {
 
   const courses = [
