f21357aa7814e0bbaaeb780953f3725ae15b53c7
Jimmy Murray <SeeJimmyMurray@gmail.com>
2024-03-19 14:42:14 -0400

    Update App.jsx

--- FullStackExercises/osa2/2.01/App.jsx
+++ FullStackExercises/osa2/2.01/App.jsx
@@ -1,1 +1,71 @@
+import React from 'react';
 
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  );
+};
+
+const Header = ({ courseName }) => {
+  return <h1>{courseName}</h1>;
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map(part => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  );
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name}
+    </p>
+  );
+};
+
+const App = () => {
+
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
