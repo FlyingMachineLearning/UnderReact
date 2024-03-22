f57aa72654be12b43db35939a9d56ca17c74f696
Jimmy Murray <SeeJimmyMurray@gmail.com>
2024-03-22 10:25:48 -0400

    Update App.jsx

--- FullStackExercises/osa2/i-2.14/App.jsx
+++ FullStackExercises/osa2/i-2.14/App.jsx
@@ -1,1 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import personsService from './services/persons';
+import Filter from './Filter';
+import PersonForm from './PersonForm';
+import Persons from './Persons';
 
+const App = () => {
+    const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
+
+
+  useEffect(() => {
+    personsService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons);
+      });
+  }, []);
+
+const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+const addPerson = (event) => {
+  event.preventDefault();
+  if (persons.some(person => person.name.toLowerCase() === newName.toLowerCase())) {
+    window.alert(`${newName} is already added to phonebook`);
+    return;
+  }
+
+
+    const personObject = { name: newName, number: newNumber };
+
+    personsService
+      .create(personObject)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson));
+        setNewName('');
+        setNewNumber('');
+      });
+  };
+
+    return (
+    <div>
+      {/* The rest of your component */}
+      <Filter searchTerm={searchTerm} handleSearchChange={handleSearchChange} />
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+      <Persons personsToShow={personsToShow} />
+    </div>
+  );
+};
+
+export default App;
