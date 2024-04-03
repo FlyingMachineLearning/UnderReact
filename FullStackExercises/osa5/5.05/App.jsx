2a906ded7fe1c22d3294aa431036160f8b5bb01e
Jimmy Murray <SeeJimmyMurray@gmail.com>
2024-04-03 14:53:31 -0400

    Update App.jsx

--- FullStackExercises/osa5/5.05/App.jsx
+++ FullStackExercises/osa5/5.05/App.jsx
@@ -1,1 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import LoginForm from './LoginForm';
+import BlogForm from './BlogForm';
+import Togglable from './Togglable'; // Import Togglable
+import Notification from './Notification';
 
+function App() {
+  // State and other component logic...
+
+  return (
+    <div>
+      <Notification message={notification?.message} type={notification?.type} />
+      {!user ? (
+        <LoginForm onLogin={handleLogin} />
+      ) : (
+        <div>
+          <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
+          
+          <Togglable buttonLabel='create new blog'>
+            <BlogForm onCreate={createBlog} />
+          </Togglable>
+          
+          {/* Display blogs */}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
