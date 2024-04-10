6e8671208e776e28082f275bb1ce4013ca5c34fb
Jimmy Murray <SeeJimmyMurray@gmail.com>
2024-04-10 11:52:01 -0400

    Update App.jsx

--- Projects/AdminDashboard/App.jsx
+++ Projects/AdminDashboard/App.jsx
@@ -1,1 +1,20 @@
 
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Dashboard from './components/FollowersGrowthDashboard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <FollowersGrowthDashboard />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
