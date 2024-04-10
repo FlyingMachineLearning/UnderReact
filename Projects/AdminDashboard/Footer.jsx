d25fff7acc1db7c620914868f81874d9d5cff2ae
Jimmy Murray <SeeJimmyMurray@gmail.com>
2024-04-10 11:50:54 -0400

    Update Footer.jsx

--- Projects/AdminDashboard/Footer.jsx
+++ Projects/AdminDashboard/Footer.jsx
@@ -1,1 +1,14 @@
 
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <p>© 2024 My Application. All rights reserved.</p>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
